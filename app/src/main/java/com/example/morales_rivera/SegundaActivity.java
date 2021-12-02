bc67af9ae001e212a96239ca0f9136382f50040f
deybi <deybirivera4@outlook.es>
2021-12-01 19:48:38 -0500

    19:45

--- app/src/main/java/com/example/morales_rivera/SegundaActivity.java
+++ app/src/main/java/com/example/morales_rivera/SegundaActivity.java
@@ -5,18 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SegundaActivity extends AppCompatActivity {
 
-    private
+    private EditText etNombre;
+    private EditText etBase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
+
+        this.etNombre=findViewById(R.id.etNombre);
+        this.etBase=findViewById(R.id.etBase);
     }
 
     public void onClickSiguiente(View view){
-        Intent intent = new Intent(this, SegundaActivity.class);
-        startActivity(intent);
+        String nombre=etNombre.getText().toString();
+        String base=etBase.getText().toString();
+
+        if(!nombre.matches("") && !base.matches("")){
+            Intent intent=new Intent(this, TerceraActivity.class);
+
+            //Enviar información desde otra activity
+            intent.putExtra("key_nombre", nombre);
+            intent.putExtra("key_base", base);
+
+
+            this.startActivity(intent);
+        }else{
+            Toast.makeText(this, "Rellenar campos obligatorios.", Toast.LENGTH_LONG).show();
+        }
     }
 }
--- app/src/main/java/com/example/morales_rivera/TerceraActivity.java
+++ app/src/main/java/com/example/morales_rivera/TerceraActivity.java
@@ -2,13 +2,56 @@ package com.example.morales_rivera;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class TerceraActivity extends AppCompatActivity {
 
+
+    private EditText etNombre;
+    private EditText etBase;
+
+    private EditText etApellido;
+    private EditText etExponente;
+    private EditText etNumero;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tercera);
+
+        etNombre = findViewById(R.id.etNombreTercera);
+
+        String nombre = getIntent().getStringExtra("key_nombre");
+        etNombre.setText(nombre.toUpperCase(Locale.ROOT));
+
+        etBase = findViewById(R.id.etBaseTercera);
+
+        String base = getIntent().getStringExtra("key_base");
+        etBase.setText(base);
+
+        //etApellido=findViewById(R.id.et)
+
+    }
+
+    public void onClickCerrar(View view){
+
+        if(!nombre.matches("") && !base.matches("")){
+            Intent intent=new Intent(this, TerceraActivity.class);
+
+            //Enviar información desde otra activity
+            intent.putExtra("key_nombre", nombre);
+            intent.putExtra("key_base", base);
+
+
+            this.startActivity(intent);
+        }else{
+            Toast.makeText(this, "Rellenar campos obligatorios.", Toast.LENGTH_LONG).show();
+        }
     }
 }
